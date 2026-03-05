117e55782556b585d69e487c1b649b651756f0ca
Sami <samishaikh77@gmail.com>
2026-03-05 18:22:32 +0530

    correction

--- src/test/java/sample_test/Demo1_test.java
+++ src/test/java/sample_test/Demo1_test.java
@@ -4,18 +4,18 @@ import org.testng.annotations.Test;
 
 public class Demo1_test {
 
-	@Test
+	@Test(priority = 1)
+	public void test_1() {
+		System.out.println("test_1 executed");
+	}
+	
+	@Test(priority = 2)
+	public void test_2() {
+		System.out.println("test_2 executed");
+	}
+	
+	@Test(priority = 3)
 	public void test_3() {
 		System.out.println("test_3 executed");
-	}
-	
-	@Test
-	public void test_4() {
-		System.out.println("test_4 executed");
-	}
-	
-	@Test
-	public void test_5() {
-		System.out.println("test_5 executed");
 	}
 }
--- src/test/java/sample_test/Demo2_Test.java
+++ src/test/java/sample_test/Demo2_Test.java
@@ -3,18 +3,18 @@ import org.testng.annotations.Test;
 
 public class Demo2_Test {
 
-	@Test
-	public void test_1() {
-		System.out.println("test_1 executed");
+	@Test(priority = 4)
+	public void test_4() {
+		System.out.println("test_4 executed");
 	}
 	
-	@Test
-	public void test_2() {
-		System.out.println("test_2 executed");
+	@Test(priority = 5)
+	public void test_5() {
+		System.out.println("test_5 executed");
 	}
 	
-	@Test
-	public void test_3() {
-		System.out.println("test_3 executed");
+	@Test(priority = 6)
+	public void test_6() {
+		System.out.println("test_6 executed");
 	}
 }
