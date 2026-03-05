fbd3724049dbb8dc38ed9aec1d460816ff5bd662
Sami <samishaikh77@gmail.com>
2026-03-05 17:44:29 +0530

    new tests

---
+++ src/test/java/Demo_Test.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+public class Demo_Test {
+
+	@Test
+	public void test_1() {
+		System.out.println("test_1 executed");
+	}
+	
+	@Test
+	public void test_2() {
+		System.out.println("test_2 executed");
+	}
+	
+	@Test
+	public void test_3() {
+		System.out.println("test_3 executed");
+	}
+}
---
+++ src/test/java/sample_test/Demo1_test.java
@@ -0,0 +1,21 @@
+package sample_test;
+
+import org.testng.annotations.Test;
+
+public class Demo1_test {
+
+	@Test
+	public void test_3() {
+		System.out.println("test_3 executed");
+	}
+	
+	@Test
+	public void test_4() {
+		System.out.println("test_4 executed");
+	}
+	
+	@Test
+	public void test_5() {
+		System.out.println("test_5 executed");
+	}
+}
