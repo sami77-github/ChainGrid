61ce6d81bdcb8c211e55597ae8c93c551fdd72ad
Sami <samishaikh77@gmail.com>
2026-03-05 18:05:11 +0530

    added tests

---
+++ src/test/java/sample_test/Demo3_test.java
@@ -0,0 +1,17 @@
+package sample_test;
+
+import org.testng.annotations.Test;
+
+public class Demo3_test {
+
+	@Test
+	public void test_7() {
+		System.out.println("test_7 executed");
+	}
+	
+	@Test
+	public void test_8() {
+		System.out.println("test_8 executed");
+	}
+	
+}
